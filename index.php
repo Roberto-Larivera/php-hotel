7647b76f3d1f0a2d66da11e1f9b1037e28af1132
Roberto Larivera <robylariv@gmail.com>
2023-02-20 17:16:48 +0100

    Aggiunta stampa in tabella

--- index.php
+++ index.php
@@ -39,8 +39,6 @@ $hotels = [
 
 ];
 
-
-
 ?>
 
 <?php  ?>
@@ -53,6 +51,7 @@ $hotels = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
 </head>
 
 <body>
@@ -70,6 +69,32 @@ $hotels = [
         }
         ?>
     </div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">name</th>
+                <th scope="col">description</th>
+                <th scope="col">parking</th>
+                <th scope="col">vote</th>
+                <th scope="col">distance_to_center</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($hotels as $hotelsKey => $hotel) {
+                echo '<tr>';
+                echo '<th scope="row">' . $hotelsKey . '</th>';
+                foreach ($hotel as $hotelKey => $hotelValue) {
+                    echo '<th>';
+                    echo $hotelValue;
+                    echo '</th>';
+                }
+                echo '</tr>';
+            }
+            ?>
+        </tbody>
+    </table>
 </body>
 
 </html>
