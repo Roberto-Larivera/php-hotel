0956c55a464dc738cd49de866e35d0877609b501
Roberto Larivera <robylariv@gmail.com>
2023-02-20 17:21:49 +0100

    Aggiunta mini-style

--- index.php
+++ index.php
@@ -55,46 +55,65 @@ $hotels = [
 </head>
 
 <body>
-
-    <div>
-        <?php
-        foreach ($hotels as $hotel) {
-            echo '<ul>';
-            foreach ($hotel as $hotelKey => $hotelValue) {
-                echo '<li>';
-                echo $hotelKey . ': ' . $hotelValue;
-                echo '</li>';
-            }
-            echo '</ul>';
-        }
-        ?>
+    <div class="container py-5">
+        <div class="row mb-5">
+            <div class="col text-center">
+                <h1>
+                    PHP Hotels
+                </h1>
+            </div>
+        </div>
+        <div class="row mb-5">
+            <div class="col">
+                <div>
+                    <?php
+                    foreach ($hotels as $hotel) {
+                        echo '<ul>';
+                        foreach ($hotel as $hotelKey => $hotelValue) {
+                            echo '<li>';
+                            echo $hotelKey . ': ' . $hotelValue;
+                            echo '</li>';
+                        }
+                        echo '</ul>';
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-5">
+            <div class="col">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Parking</th>
+                            <th scope="col">Vote</th>
+                            <th scope="col">Distance to center</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($hotels as $hotelsKey => $hotel) {
+                            echo '<tr>';
+                            echo '<th scope="row">' . $hotelsKey . '</th>';
+                            foreach ($hotel as $hotelKey => $hotelValue) {
+                                echo '<th>';
+                                echo $hotelValue;
+                                echo '</th>';
+                            }
+                            echo '</tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">name</th>
-                <th scope="col">description</th>
-                <th scope="col">parking</th>
-                <th scope="col">vote</th>
-                <th scope="col">distance_to_center</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            foreach ($hotels as $hotelsKey => $hotel) {
-                echo '<tr>';
-                echo '<th scope="row">' . $hotelsKey . '</th>';
-                foreach ($hotel as $hotelKey => $hotelValue) {
-                    echo '<th>';
-                    echo $hotelValue;
-                    echo '</th>';
-                }
-                echo '</tr>';
-            }
-            ?>
-        </tbody>
-    </table>
+
+
+
 </body>
 
 </html>
